fe9f70fcc151cc7887e3989d9c5d2928e21dce95
Kuba Trzaskoma <jt1@o2.pl>
2012-12-23 18:28:28 +0100

    [IMP] Add Meteoblue support
    * Site per class
    * Sender per class
    * Automatic site detection based on url

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ import logging
 import datetime
 from google.appengine.ext import db
 from model import Task
-from sites import Yr
 from sms import GSMPortal
+from sites import Yr, Meteoblue
 
 
 class SendSMS(webapp2.RequestHandler):
     def __init__(self, request, response):
         self.initialize(request, response)
-        self.sites = {"yr": Yr}
+        self.sites = {"yr": Yr, "meteoblue": Meteoblue}
         self.smsSender = GSMPortal()
 
     def get(self):
-
         for task in db.GqlQuery("SELECT * FROM Task WHERE sendDateTimeList != NULL"):
             sendDateTimeToDelete = list()
             for sendDateTime in task.sendDateTimeList:
                 if sendDateTime < datetime.datetime.now() + datetime.timedelta(hours=1):
-                    site = self.sites[task.site](task.url, task.periods)
+                    for siteCand in self.sites:
+                        if siteCand in task.url:
+                            site = self.sites[siteCand](task.url, task.periods)
                     if self._sendSMS(self.smsSender, site, task.phone):
                         sendDateTimeToDelete.append(sendDateTime)
             task.sendDateTimeList = filter(lambda x: x not in sendDateTimeToDelete, task.sendDateTimeList)
@@ -43,8 +44,7 @@ class SendSMS(webapp2.RequestHandler):
 
 class AddTask(webapp2.RequestHandler):
     def post(self):
-        task = Task(site=self.request.get('site').strip(),
-                    phone=self.request.get('phone').strip(),
+        task = Task(phone=self.request.get('phone').strip(),
                     url=self.request.get('url').strip(),
                     periods=map(
                     int, self.request.get('periods').split(';')),
@@ -57,19 +57,17 @@ class TaskList(webapp2.RequestHandler):
     def get(self):
         self.response.out.write('<html><body><b>All tasks:</b><br>')
         for task in db.GqlQuery("SELECT * FROM Task"):
-            self.response.out.write('site: {}, phone: {}, periods: {}, sendDateTimeList: {}, url: {}<br>'.format(task.site, task.phone, task.periods, task.sendDateTimeList, task.url))
-        self.response.out.write("""<br><b>New task:</b><br>
-              <form action="/addTask" method="post">
-                <div>site:
-                    <input type="radio" name="site" value="yr" checked>yr.no
-                    <input type="radio" name="site" value="meteoblue">meteoblue.com
-                </div>
+            self.response.out.write('phone: {}, periods: {}, sendDateTimeList: {}, url: {}<br>'.format(task.phone, task.periods, task.sendDateTimeList, task.url))
+        self.response.out.write("""
+            <form action="/addTask" method="post">
+                <div><input type="button" value="Send now" onclick="location.href='/send';"></div>
+                <br><b>New task:</b><br>
                 <div>url: <input type="text" name="url" size="80" value="http://www.yr.no/place/France/Rh%C3%B4ne-Alpes/Chamonix/varsel.xml"></input></div>
                 <div>phone: <input type="text" name="phone" value="48123456" ></input></div>
-                <div>periods: <input type="text" name="periods" value="1;2;3"></input></div>
-                <div>send date/time list:</div>
+                <div>periods: <input type="text" name="periods" value="0;1;2;3"></input></div>
+                <div>send date/time list (use ';' as a separator):</div>
                 <div><textarea name="sendDateTimeList" rows="10" cols="65">""" + (datetime.datetime.now() + datetime.timedelta(hours=1)).strftime("%d-%m-%Y %H:%M") + """</textarea></div>
-                <div><input type="submit" value="Add task"> <input type="button" value="Send now" onclick="location.href='/send';"></div>
+                <div><input type="submit" value="Add task"></div>
               </form>
             </body>
           </html>""")
--- sites.py
+++ sites.py
@@ -6,10 +6,7 @@ from bs4 import BeautifulSoup
 class WeatherSite(object):
     def __init__(self, url, periods):
         super(WeatherSite, self).__init__()
-        self.url = url
         self.periods = periods
-        self.xml = BeautifulSoup(urllib2.urlopen(url))
-        self.periodPattern = '{start}-{end} {symbol} {precipitation}mm {wind}m/s {temp}C '
 
     def generateText(self):
         pass
@@ -18,15 +15,37 @@ class WeatherSite(object):
 class Yr(WeatherSite):
     def __init__(self, url, periods):
         super(Yr, self).__init__(url, periods)
+        self.xml = BeautifulSoup(urllib2.urlopen(url))
+        self.periodPattern = '{start}-{end} {symbol} {precipitation}mm {wind}m/s {temp}C '
 
     def generateText(self):
         text = self.xml.weatherdata.location.contents[1].contents[0] + ' '
-        for i, period in enumerate(self.xml.find_all('time'), start=1):
-            if i in self.periods:
-                text += self.periodPattern.format(start=period.get('from')[8:13],
-                                                  end=period.get('to')[11:13],
-                                                  symbol=period.symbol.get('name'),
-                                                  precipitation=period.precipitation.get('value'),
-                                                  wind=period.windspeed.get('mps'),
-                                                  temp=period.temperature.get('value'))
+        dataPeriods = self.xml.find_all('time')
+        for i in self.periods:
+            text += self.periodPattern.format(start=dataPeriods[i].get('from')[8:13],
+                                              end=dataPeriods[i].get('to')[11:13],
+                                              symbol=dataPeriods[i].symbol.get('name'),
+                                              precipitation=dataPeriods[i].precipitation.get('value'),
+                                              wind=dataPeriods[i].windspeed.get('mps'),
+                                              temp=dataPeriods[i].temperature.get('value'))
+        return text.encode('latin-1')
+
+class Meteoblue(WeatherSite):
+    def __init__(self, url, periods):
+        super(Meteoblue, self).__init__(url, periods)
+        opener = urllib2.build_opener()
+        opener.addheaders.append(('Cookie', 'speed=METER_PER_SECOND;temp=CELSIUS'))
+        self.xml = BeautifulSoup(opener.open(url))
+        self.periodPattern = 'T{hour} {precipitation}mm ({probability}) {wind}m/s {temp}C '
+
+    def generateText(self):
+        text = self.xml.find(id='location_info').h1.string.replace('Weather', '') + ' ' + self.xml.find(class_='picto').find_all("tr")[0].th.string.split('.')[1][0:2] + ' '
+        trs = self.xml.find(class_='picto').find_all("tr")
+        for i in self.periods:
+            text += self.periodPattern.format(hour=trs[0].find_all("td")[i].string.string[0:2],
+                                              symbol=trs[1].find_all("td")[i]['class'][2],
+                                              precipitation=trs[6].find_all("td")[i].string,
+                                              probability=trs[7].find_all("td")[i].string,
+                                              wind=trs[5].find_all("td")[i].string,
+                                              temp=trs[2].find_all("td")[i].string.split('\t')[0])
         return text.encode('latin-1')
